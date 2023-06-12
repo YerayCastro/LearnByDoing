4c6dc4514e5d2f1bb6933d16487ccfed86ec5bdb
YerayCastro <112658475+YerayCastro@users.noreply.github.com>
2023-06-12 14:37:58 +0200

    commit

---
+++ Learn by Doing/Learn by Doing/Learn_by_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_by_DoingApp.swift
+//  Learn by Doing
+//
+//  Created by Yery Castro on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Learn_by_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
